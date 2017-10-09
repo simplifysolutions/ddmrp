0225d5c2be76b6aa2a192c43b47b00b4f18c8264
John Boyle <john.boyle@simplifycloud.com>
2017-10-09 21:10:06 +0000

    8.0 Backport fixups

--- ddmrp/models/stock_warehouse_orderpoint.py
+++ ddmrp/models/stock_warehouse_orderpoint.py
@@ -110,6 +110,41 @@ class StockWarehouseOrderpoint(models.Model):
             rec.procure_recommended_date = \
                 fields.date.today() + timedelta(days=int(rec.dlt))
 
+    # Pulled from 9.0
+    def subtract_procurements_from_orderpoints(self, cr, uid, orderpoint_ids, context=None):
+        '''This function returns quantity of product that needs to be deducted from the orderpoint computed quantity because there's already a procurement created with aim to fulfill it.
+        '''
+
+        cr.execute("""select op.id, p.id, p.product_uom, p.product_qty, pt.uom_id, sm.product_qty from procurement_order as p left join stock_move as sm ON sm.procurement_id = p.id,
+                                    stock_warehouse_orderpoint op, product_product pp, product_template pt
+                                WHERE p.orderpoint_id = op.id AND p.state not in ('done', 'cancel') AND (sm.state IS NULL OR sm.state not in ('draft'))
+                                AND pp.id = p.product_id AND pp.product_tmpl_id = pt.id
+                                AND op.id IN %s
+                                ORDER BY op.id, p.id
+                    """, (tuple(orderpoint_ids),))
+        results = cr.fetchall()
+        current_proc = False
+        current_op = False
+        uom_obj = self.pool.get("product.uom")
+        op_qty = 0
+        res = dict.fromkeys(orderpoint_ids, 0.0)
+        for move_result in results:
+            op = move_result[0]
+            if current_op != op:
+                if current_op:
+                    res[current_op] = op_qty
+                current_op = op
+                op_qty = 0
+            proc = move_result[1]
+            if proc != current_proc:
+                op_qty += uom_obj._compute_qty(cr, uid, move_result[2], move_result[3], move_result[4], round=False)
+                current_proc = proc
+            if move_result[5]: #If a move is associated (is move qty)
+                op_qty -= move_result[5]
+        if current_op:
+            res[current_op] = op_qty
+        return res
+
     @api.multi
     @api.depends("net_flow_position", "dlt", "adu",
                  "buffer_profile_id.lead_time_id.factor",
